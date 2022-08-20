431fdf38dc23aca4eafc91ccbfd3c5c67ae1a341
alfa6884 <96798193+alfa6884@users.noreply.github.com>
2022-08-20 20:20:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask,render_template,request,redirect,flash,url_for
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 import pickle as pk
 
 app = Flask(__name__)
