30c12887ec610e3f2f7d7943b1ace8a4cfd234e2
alfa6884 <96798193+alfa6884@users.noreply.github.com>
2022-08-20 20:24:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 import pickle as pk
 
 app = Flask(__name__)
-app.secret_key = "hello"
-@app.route('/', methods=['POST','GET'])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
     if request.method == 'POST':
         noc = request.form['course_no']
         ts = request.form['time_s']
@@ -12,7 +15,8 @@ def index():
         features = [float(i) for i in c]
         model = pk.load(open('model.pkl','rb'))
         pred = model.predict([features])
-        flash(pred)
-    return render_template('index.html')
+        return render_template('predict.html', pred = pred)
+        
+
 if __name__ == "__main__":
     app.run(debug=False)
