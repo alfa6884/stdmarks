f1eb1627af2e2574ae9a1076102c5698a15a9a00
alfa6884 <96798193+alfa6884@users.noreply.github.com>
2022-08-21 01:47:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 import pickle as pk
 
 app = Flask(__name__)
+app.secret_key = "scretkey123" 
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -16,7 +17,8 @@ def predict():
         features = [float(i) for i in c]
         model = pk.load(open('model.pkl','rb'))
         pred = model.predict([features])
-        return render_template('index.html', pred = pred)
+        flash(pred)
+        return render_template('index.html')
         
 
 if __name__ == "__main__":
