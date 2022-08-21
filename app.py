28f8af0ffc06ccc6f2075ddc13ded7382df9627b
alfa6884 <96798193+alfa6884@users.noreply.github.com>
2022-08-21 01:59:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 import pickle as pk
 
 app = Flask(__name__)
-app.secret_key = "scretkey123" 
+#app.secret_key = "scretkey123" 
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,9 +14,9 @@ def predict():
         noc = request.form['course_no']
         ts = request.form['time_s']
         c = [noc,ts]
-        features = [float(i) for i in c]
+        mark = [float(i) for i in c]
         model = pk.load(open('model.pkl','rb'))
-        pred = model.predict([features])
+        pred = model.predict([mark])
         flash(pred)
         return render_template('index.html')
         
